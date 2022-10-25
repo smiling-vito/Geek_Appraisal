d2710e3a2da61fdb0a40036d25cadf646b2c7aec
zhangjincan <2302688467@qq.com>
2022-10-25 23:58:22 +0800

    Add main.c xiugaihou

--- task1/main.c
+++ task1/main.c
@@ -1,27 +1,31 @@
-#include<stdio.h>
-void Euler(double a)
-{
- double n = 0.001;
- 
- while (1)
- {
- 	
-     double b = 0.999 * a;
-  printf("%lf      %lf\n", n, b);
-  if(a-b<0.000001)
-  {
-   break;
-  }
-  a = b; 
-  n += 0.001;
- }
-
-}
+#include <stdio.h> 
+#include <math.h>
 int main()
-{
- int a;
- scanf("%d", &a);
- printf("       x       y\n");
- Euler(a);
- return 0;
+{double h;
+double a;
+double y;
+h=0.001;
+a=1;
+a=exp(a);
+printf("%f",a);
+double z=a-y;
+int n=0;
+double t=0;
+double w;
+double m;
+m=25;
+m=exp(1)*m;
+printf("%f\n",m);
+while(z>0.001*h&&w<m){y=a-a*h;
+w=t+h;
+t=w;
+z=a-y;
+a=y;
+n++;
+printf("x=%f      f(x%d)=%f      f(x%d)-f(x%d)=%f\n",w,n,y,n,n-1,z);
 }
+
+printf("zuizhongdingtai=%f xquzhi%f",y,w);
+return 0;
+}
+
